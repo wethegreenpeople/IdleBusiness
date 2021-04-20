56b91703fa9da47eb8b340b33ba3971db62578b9
John Singh <wethegreenpeople@gmail.com>
2021-04-20 14:40:24 -0700

    Include sector when fetching business from API

--- IdleBusiness/Api/Controllers/BusinessController.cs
+++ IdleBusiness/Api/Controllers/BusinessController.cs
@@ -45,6 +45,7 @@ namespace IdleBusiness.Api.Controllers
                     .ThenInclude(s => s.Investment)
                         .ThenInclude(s => s.BusinessInvestments) // this is the level that contains both ends of the investment
                 .Include(s => s.GroupInvestments)
+                .Include(s => s.Sector)
                 .SingleOrDefaultAsync(s => s.Id == Convert.ToInt32(businessId));
             business.BusinessScore = business.Owner.Score;
 
